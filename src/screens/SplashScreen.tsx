8d701093a9b47bf3352511dce8fe859dfb387517
nome <email>
2026-04-22 20:17:44 -0300

    fix: Image width/height percent strings incompatible with RN Web

--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -96,7 +96,7 @@ export function SplashScreen({ onFinish }: Props) {
         }}>
           <Image
             source={require('../../assets/logo-pintura.png')}
-            style={{ width: '100%', height: '100%' }}
+            style={{ width: 180, height: 180 }}
             resizeMode="cover"
           />
         </View>
